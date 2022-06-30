944548d935da29800a2fb91b73f6f8411979c8ab
SheeshMirza <msmirza.in@gmail.com>
2022-06-30 19:20:03 +0530

    Number dilemma

---
+++ Solutions/Number dilemma/Main.cpp
@@ -0,0 +1,9 @@
+int StringToInt(string S){
+    int result = stoi(S);
+    return result;
+}
+
+string IntToString(int N){
+    string result = to_string(N);
+    return result;
+}
---
+++ Solutions/Number dilemma/Main.java
@@ -0,0 +1,9 @@
+static int StringToInt(String S){
+    int value = Integer.valueOf(S);
+    return value;
+}
+
+static String IntToString(int N){
+    String value = String.valueOf(N);
+    return value;
+}
