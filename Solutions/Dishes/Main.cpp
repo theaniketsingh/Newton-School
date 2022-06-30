c77ce7327b9f72dfccebb3631e0ed18aa66e35a4
SheeshMirza <msmirza.in@gmail.com>
2022-06-30 19:30:05 +0530

    Dishes

---
+++ Solutions/Dishes/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N, T;
+    cin >> N >> T;
+    cout << T-N;
+    return 0;
+}
---
+++ Solutions/Dishes/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int T = sc.nextInt();
+        System.out.print(T-N);
+    }
+}
