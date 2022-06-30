6c4723247844ec49f45e32b620e668d2f93fc73f
SheeshMirza <msmirza.in@gmail.com>
2022-06-30 19:30:03 +0530

    Print the input!

---
+++ Solutions/Print the input!/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.print(number);
+    }
+}
